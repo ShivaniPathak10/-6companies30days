33b299bba4967cca87e6d5702072768dfa5b18d0
Shivani <pathakshivani843@gmail.com>
2025-01-17 23:01:18 +0530

    Problem-50

---
+++ Maximum-product-after-K-increments.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums, int k) {
+        const int mod=1e9+7;
+        priority_queue<int, vector<int>,greater<int>> pq;
+        for(auto i: nums)
+        {
+            pq.push(i);
+        }
+        while(k>0)
+        {
+            int x=pq.top();
+            pq.pop();
+            pq.push(x+1);
+            k--;
+        }
+        long long ans=1;
+        while(!pq.empty())
+        {
+            long long x=pq.top();
+            ans*=x;
+            ans%=mod;
+            pq.pop();
+        }
+        return ans;
+    }
+};
